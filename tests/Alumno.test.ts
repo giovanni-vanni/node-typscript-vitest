6b9d5a50cf57ad8aa7a608656cb855a5a561bea0
giovanni-vanni <giovannivanni20@gmail.com>
2026-08-19 15:17:58 -0300

    añadir legajo y test

--- tests/Alumno.test.ts
+++ tests/Alumno.test.ts
@@ -4,21 +4,33 @@ import { Alumno } from "../src/Alumno";
 describe("Alumno", () => {
 
   test("un alumno de 18 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Juan", 18);
+    const alumno = new Alumno("Juan", 18, 12345);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
   test("un alumno de 25 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Ana", 25);
+    const alumno = new Alumno("Ana", 25, 12345);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
   test("un alumno de 17 años no debe ser mayor de edad", () => {
-    const alumno = new Alumno("Pedro", 17);
+    const alumno = new Alumno("Pedro", 17, 12345);
 
     expect(alumno.esMayorDeEdad()).toBe(false);
   });
 
+});
+
+describe("Alumno - obtenerEstado", () => {
+    test("un alumno de 18 años debe ser mayor de edad", () => {
+        const alumno = new Alumno("Juan", 18, 12345);
+        expect(alumno.obtenerEstado()).toBe("Mayor de edad");
+    });
+
+    test("un alumno de 17 años no debe ser mayor de edad", () => {
+        const alumno = new Alumno("Pedro", 17, 12345);
+        expect(alumno.obtenerEstado()).toBe("Menor de edad");
+    });
 });
