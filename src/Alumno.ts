2b5604a2146b60657b99722d487c79a69c94a171
giovanni-vanni <giovannivanni20@gmail.com>
2026-08-19 17:27:40 -0300

    clase persona

--- src/Alumno.ts
+++ src/Alumno.ts
@@ -1,9 +1,12 @@
-export class Alumno {
+import { Persona } from "./Persona";
+export class Alumno extends Persona {
   constructor(
     public nombre: string,
     public edad: number,
     public legajo: number
-  ) {}
+  ) {
+      super(nombre, edad, legajo);
+  }
 
   esMayorDeEdad(): boolean {
     return this.edad >= 18;
--- src/Profesor.ts
+++ src/Profesor.ts
@@ -1,8 +1,12 @@
-export class Profesor {
+import { Persona } from "./persona";
+
+export class Profesor extends Persona {
     constructor(
         public nombre: string,
         public legajo: number,
-    ) {}
+    ) {
+        super(nombre, 0, legajo);
+    }
 
     obtenerNombreCompleto(): string {
         return this.nombre;
